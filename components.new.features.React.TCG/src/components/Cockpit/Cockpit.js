c7446540254609f4e2ba77138dd1ddc5a88935ba
gogolab <m4ttek@gmail.com>
2019-02-08 19:17:17 +0100

    useEffects hook inro

--- components.new.features.React.TCG/src/components/Cockpit/Cockpit.js
+++ components.new.features.React.TCG/src/components/Cockpit/Cockpit.js
@@ -1,28 +1,52 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
-import classes from './Cockpit.css';
+import classes from "./Cockpit.css";
 
-const cockpit = ( props ) => {
+const cockpit = props => {
+    // useEffect(() => {
+    //     console.log("[Cockpit.js] useEffect");
+
+    //     setTimeout(() => {
+    //         alert("simulate e.g. http request");
+    //     }, 1000);
+    // }); // runs every time
+
+    // useEffect(() => {
+    //     console.log("[Cockpit.js] useEffect");
+
+    //     setTimeout(() => {
+    //         alert("simulate e.g. http request");
+    //     }, 1000);
+    // }, [props.persons]); // runs only when props.persons change
+
+    useEffect(() => {
+        console.log("[Cockpit.js] useEffect");
+
+        setTimeout(() => {
+            alert("simulate e.g. http request");
+        }, 1000);
+    }, []); // runs only once, empty array means no dependencies, but prevents from running every time
+
     const assignedClasses = [];
-    let btnClass = '';
+    let btnClass = "";
     if (props.showPersons) {
         btnClass = classes.Red;
     }
 
-    if ( props.persons.length <= 2 ) {
-      assignedClasses.push( classes.red ); // classes = ['red']
+    if (props.persons.length <= 2) {
+        assignedClasses.push(classes.red); // classes = ['red']
     }
-    if ( props.persons.length <= 1 ) {
-      assignedClasses.push( classes.bold ); // classes = ['red', 'bold']
+    if (props.persons.length <= 1) {
+        assignedClasses.push(classes.bold); // classes = ['red', 'bold']
     }
 
     return (
         <div className={classes.Cockpit}>
             <h1>{props.title}</h1>
-            <p className={assignedClasses.join( ' ' )}>This is really working!</p>
-            <button
-                className={btnClass}
-                onClick={props.clicked}>Toggle Persons</button>
+            <p className={assignedClasses.join(" ")}>This is really working!</p>
+            <button className={btnClass} onClick={props.clicked}>
+                Toggle Persons
+            </button>
         </div>
     );
 };
